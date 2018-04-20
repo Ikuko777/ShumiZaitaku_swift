dcc0040473049bd172e1b889d5a22706e50d6c23
IkukoHiraga <Ikuko@Ikuko-H.local>
2018-04-20 15:44:57 +0900

    my first project

---
+++ ShumiZaitaku_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShumiZaitaku_swift
+//
+//  Created by IkukoHiraga on 2018/04/20.
+//  Copyright © 2018年 IkukoHiraga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
